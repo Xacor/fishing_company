eb3aa2da31d57fa3efb762d6a42b7f216e9f2980
Рябов Константин Дмитриевич <ryabov.kostantin18@gmail.com>
2022-09-25 22:10:58 +0300

    refactor/changed config loading

--- cmd/main.go
+++ cmd/main.go
@@ -2,31 +2,24 @@ package main
 
 import (
 	"fishing_company/pkg/common/boats"
+	"fishing_company/pkg/common/config"
 	"fishing_company/pkg/common/db"
 
 	"github.com/gin-gonic/gin"
-	"github.com/spf13/viper"
 )
 
 func main() {
-	viper.SetConfigFile("./pkg/common/envs/.env")
-	viper.ReadInConfig()
-
-	port := viper.Get("PORT").(string)
-	dbUrl := viper.Get("DB_URL").(string)
+	conf, err := config.LoadConfig()
+	if err != nil {
+		panic(err.Error())
+	}
 
 	router := gin.Default()
 	router.LoadHTMLGlob("ui/html/**/*")
 
-	handler := db.Init(dbUrl)
+	handler := db.Init(conf.DBUrl)
 
 	boats.RegisterRoutes(router, handler)
 
-	// func(c *gin.Context) {
-	// 	c.JSON(200, gin.H{
-	// 		"port":  port,
-	// 		"dbUrl": dbUrl,
-	// 	})
-	// }
-	router.Run(port)
+	router.Run(conf.Port)
 }
--- pkg/common/config/config.go
+++ pkg/common/config/config.go
@@ -1,6 +1,10 @@
 package config
 
-import "github.com/spf13/viper"
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
 
 type Config struct {
 	Port  string `mapstructure:"PORT"`
@@ -11,16 +15,18 @@ func LoadConfig() (c Config, err error) {
 	viper.AddConfigPath("./pkg/common/config/envs")
 	viper.SetConfigName("dev")
 	viper.SetConfigType("env")
-
 	viper.AutomaticEnv()
 
 	err = viper.ReadInConfig()
-
 	if err != nil {
-		return
+		err = fmt.Errorf("fatal error config file: %w", err)
+		return Config{}, err
 	}
 
 	err = viper.Unmarshal(&c)
-
+	if err != nil {
+		err = fmt.Errorf("unable to decode into struct, %v", err)
+		return Config{}, err
+	}
 	return
 }
